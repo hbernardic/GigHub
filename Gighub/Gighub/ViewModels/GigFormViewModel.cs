231bb8218b808fbcdfa00c07c5131f1a2cd2018e
hbernardic <hbernardic@gmail.com>
2017-02-09 20:48:19 +0100

    create new gig and server validation

--- Gighub/Gighub/ViewModels/GigFormViewModel.cs
+++ Gighub/Gighub/ViewModels/GigFormViewModel.cs
@@ -1,14 +1,31 @@
-﻿using System.Collections.Generic;
-using Gighub.Models;
+﻿using Gighub.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gighub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
-        public int Genre { get; set; }
+
+        [Required]
+        public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse($"{Date} {Time}");
+        }
     }
 }
